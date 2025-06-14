c4050e27c80572e75f010b1742d3bdb759f6a70c
ilsaf <ilsaftoday@gmail.com>
2025-06-14 14:39:53 +0300

    init: add Vite usage

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,10 @@
 <template>
   <div id="app">
-    <h1>{{ backendMessage }}</h1>
+    <h1>Hello World</h1>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-
-export default {
-  name: 'App',
-  data() {
-    return {
-      backendMessage: 'Hello World',
-    };
-  },
-  mounted() {
-    axios.get('/api/message')
-      .then(response => {
-        this.backendMessage = response.data.message;
-      })
-      .catch(error => {
-        console.error('Error fetching message from backend:', error);
-      });
-  },
-};
 </script>
 
 <style>
